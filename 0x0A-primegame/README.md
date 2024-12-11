83de72dae8b122711744078e39145362dcce32a7
khumopaul <u15190219@tuks.co.za>
2024-12-12 01:51:02 +0200

    0-prime_game.py

---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,2 @@
+0x0A-primegame
+All the tasks to be followed numerically
---
+++ 0x0A-primegame/main_0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+isWinner = _import_('0-prime_game').isWinner
+
+
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
